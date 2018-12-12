f37f4120072b803af4aa63d2fb627b8f3231f817
Greg Morgan <gregm9999@gmail.com>
2018-12-12 18:12:17 +0000

    yes

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2015_12_01_201114) do
+ActiveRecord::Schema.define(version: 20181212170634) do
 
   create_table "articles", force: :cascade do |t|
-    t.string "title"
+    t.string "name"
     t.string "content"
   end
 
