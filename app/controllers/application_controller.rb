f38ed08e42d0d39645122808607fe9cdae6b016e
Greg Morgan <gregm9999@gmail.com>
2018-12-12 06:34:12 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,3 @@
-
 require_relative '../../config/environment'
 
 class ApplicationController < Sinatra::Base
@@ -10,4 +9,44 @@ class ApplicationController < Sinatra::Base
 
   get '/' do
   end
+
+  get '/articles/new' do
+    erb :new
+  end
+
+  post '/articles' do
+    article=Article.new({name: params[:name], content: params[:content]})
+    article.save
+    redirect '/articles'
+  end
+
+  get '/articles' do
+    @articles=Article.all
+    erb :index
+  end
+
+  get '/articles/:id/edit' do
+    @article=Article.find(params[:id].to_i)
+    erb :edit
+  end
+
+  patch '/articles/:id' do
+    @article=Article.find(params[:id].to_i)
+    @article.update(name: params[:name])
+    @article.update(content: params[:content])
+    @article.save
+    redirect to 'articles/'+article.id.to_s
+  end
+
+  get '/articles/:id' do
+    @article=Article.find(params[:id].to_i)
+    erb :show
+  end
+
+  delete '/articles/:id/delete' do
+    @article=Article.find(params[:id].to_i)
+    @article.destroy
+    erb :delete
+  end
+
 end
